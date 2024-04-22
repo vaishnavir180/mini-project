63ddbe2f2a9e557091930f8d053b056e2c395aec
vaishnavir180 <159414871+vaishnavir180@users.noreply.github.com>
2024-04-22 13:00:16 +0530

    Update login.js

--- login.js
+++ login.js
@@ -2,15 +2,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getAuth, signInWithEmailAndPassword, sendPasswordResetEmail } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+ const firebaseConfig = {
     apiKey: "AIzaSyB11_21N9mfkaG_Z62pFprwO_h-w8ekrQs",
     authDomain: "compiler-8e073.firebaseapp.com",
     projectId: "compiler-8e073",
     storageBucket: "compiler-8e073.appspot.com",
     messagingSenderId: "1070161979004",
     appId: "1:1070161979004:web:11c7f94b909d65c4eb8bf9"
-};
-
+  };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
