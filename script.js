2f09d8aa9448677b2689896c1e73f76f22ddfc3b
vaishnavir180 <159414871+vaishnavir180@users.noreply.github.com>
2024-04-22 13:00:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 // Your web app's Firebase configuration
-const firebaseConfig = {
+ const firebaseConfig = {
     apiKey: "AIzaSyB11_21N9mfkaG_Z62pFprwO_h-w8ekrQs",
     authDomain: "compiler-8e073.firebaseapp.com",
     projectId: "compiler-8e073",
     storageBucket: "compiler-8e073.appspot.com",
     messagingSenderId: "1070161979004",
     appId: "1:1070161979004:web:11c7f94b909d65c4eb8bf9"
-};
-
+  };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
